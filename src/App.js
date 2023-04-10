3ec998aed15f7609b30bffcf06081cb3ccd2b5aa
omaralim111 <50689130+omaralim111@users.noreply.github.com>
2023-04-10 11:28:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={moon} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://omaralim.eth.limo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Omar Alim
+        </a>
       </header>
     </div>
   );
