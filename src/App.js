3c4ee9afe845f836f50204f04436cfb7b2ecbe2d
omaralim111 <50689130+omaralim111@users.noreply.github.com>
2023-04-11 15:52:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import moon from './moon';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={moon} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
