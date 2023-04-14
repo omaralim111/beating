75c4197d806d6b7d225ee4a299f9a3c71c251649
omaralim111 <50689130+omaralim111@users.noreply.github.com>
2023-04-14 17:35:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={moon} className="App-logo" alt="moon" />
         <p>      
-          <h2> play </h2>
+         <audio src={balance} className="App-logo" alt="moon" />
         </p>
         <p>
          Ramadan <code>Mubarak</code> from
