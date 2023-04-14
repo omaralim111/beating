01d34adf4b39d52f5b1ebb912d67fc23e7d7716d
omaralim111 <50689130+omaralim111@users.noreply.github.com>
2023-04-14 17:58:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={moon} className="App-logo" alt="moon" />
         <p>      
-         <button src={balance} className="App-logo" alt="moon" />
+         <button src={balance} className="App-button" alt="moon" />
         </p>
         <p>
          Ramadan <code>Mubarak</code> from
