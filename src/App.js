64f961a54c06ab5f4e7982d70565d9ce45857888
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-04-28 00:15:28 -0400

    Update App.js
    Adding The MetaMask Button

--- src/App.js
+++ src/App.js
@@ -6,20 +6,37 @@ import './App.css';
 
 function App() {
   let audio = new Audio("/Omar_Balance.mp3")
-  
   const start = () => {
     audio.play()
   }
-
+  const connectWallet = async() => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined"){
+      try {
+        /* MetaMask is installed */
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
+        console.log(accounts[0]);
+      }catch(err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed*/
+      console.log("Please Install MetaMask")
+    }
+  }
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={moon} className="App-logo" alt="moon" />
+        <p>
+        <button onClick={connectWallet}>Connect Wallet</button>
+        </p>
         <p>      
         <button onClick={start}>Play</button>
         </p>        
         <p>
-         Art  <code>  Officially </code> Intelligent
+        Art  <code>  Officially </code> Intelligent
         </p>
         <a
           className="App-link"
