2e4db6448d6f69c2aa1e8021421b00e4e673fd1b
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-05-01 16:17:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined"){
       try {
         /* MetaMask is installed */
-        const accounts = await window.ethereum.request({ method: "eth_accounts"});
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
         console.log(accounts[0]);
       }catch(err) {
         console.error(err.message);
