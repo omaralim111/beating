0119c7213e7b86de58dffcf2ddac07f7387b8713
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-05-05 17:00:02 -0400

    up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ethers, from 'ethers';
-import react, {useState} from 'react';
+/*import ethers, from 'ethers';
+import react, {useState} from 'react';*/
 import react,{Component} from 'react';
 import logo from './logo.svg';
 import balance from './Omar_Balance.mp3';
