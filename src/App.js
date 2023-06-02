b3b91a0d26d143724792aa0dcc8f867c75450400
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-06-02 00:11:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const connectWallet = async() => {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined"){
       try {
+        /* MetaMask is not installed */
         const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
         setwalletAddress(accounts[0]);
         console.log(accounts[0]);
