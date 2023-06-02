bc97006b02e6666577ba24ffab10f1bfb7c57ccb
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-06-02 17:36:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import TxList from "./TxList";*/
 const sendTransaction = async ({ setError, setTxs, ether, addr }) => {
   try {
     from: accounts[0], // The user's active address.
-    to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970', // Required except during contract publications.
+    to: '0x058508d2ACFe10652072948A60B7286bd1acEC9d', // Required except during contract publications.
     value: '0x29a2241af62c0000', // Only required to send ether to the recipient from the initiating external account.
     gasPrice: '0x09184e72a000', // Customizable by the user during MetaMask confirmation.
     gas: '0x2710', 
