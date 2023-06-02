4e0ca9bc7c77da70c1d4febfd97ed5545829830e
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-06-02 16:39:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import React from 'react';
 import ErrorMessage from "./ErrorMessage";
 import TxList from "./TxList";
 
-const sendTransaction = async ({ setError, setTxs, ether, addr }) => {
+/*const sendTransaction = async ({ setError, setTxs, ether, addr }) => {
   try {
+    from: accounts[0], // The user's active address.
+    to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970', // Required except during contract publications.
+    value: '0x29a2241af62c0000', // Only required to send ether to the recipient from the initiating external account.
+    gasPrice: '0x09184e72a000', // Customizable by the user during MetaMask confirmation.
+    gas: '0x2710', 
     console.log({ ether, addr });
   } catch (err){
     setError(err.message);
   }
-};
+};*/
 
 function App() {
   let audio = new Audio("/Omar_Balance.mp3")
   const start = () => {
     audio.play()
   }
-  const [error, setError] = useState("");
-  const [txs, setTxs] = useState([]);
+  /*const [error, setError] = useState("");
+  const [txs, setTxs] = useState([]);*/
   const [signer, setSigner] = useState("");
   const [ walletAddress, setwalletAddress] = useState("");
   const connectWallet = async() => {
