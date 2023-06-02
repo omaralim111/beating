66d74b9f4b02d3bddd627657600832422f70e8c0
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-06-02 18:23:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
         const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
         /*const provider = new ethers.providers(window.ethereum);*/
         setwalletAddress(accounts[0]);
-        setprovider(accounts[0]);
         setSigner(accounts[0]);
         console.log(accounts[0]);
       } catch(err) {
