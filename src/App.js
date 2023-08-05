9ff584d5f81002e92d953b1caf0d535bf7c4f670
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-08-05 17:31:23 -0400

    Otter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import { useEffect, useState} from'react';
 import balance from './Omar_Balance.mp3';
 import moon from './moon.png';
+import otter from './IMG-0096.jpg';
 import './App.css';
 import React from 'react';
 import ErrorMessage from "./ErrorMessage";
