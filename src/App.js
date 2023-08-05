54eaed937168eaea25186c8fe635d9e7371ed535
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-08-05 16:26:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       try {
         /* MetaMask is not installed */
         const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
-        const signer = new window.ethereum.request({method: 'eth_sendTransaction',params:[tx]});
+        const accounts = new window.ethereum.request({method: 'eth_sendTransaction',params:[tx]});
         const transaction = new signer.sendTransaction(tx);
         setwalletAddress(accounts[0]);
         setSigner(accounts[0]);
