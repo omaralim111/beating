7bbfde1951fe2ca8399037f97858d0d494be0768
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-08-13 19:35:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ function App() {
   }
   const sendTransaction = async() => {
     let params = [{
-      from: "(accounts[0])",
-      to: "0x058508d2ACFe10652072948A60B72860B7286bd1acEC9d",
-      value: Number(50000000000000).toString(16),
-      gasPrice: Number(2500000).toString(16),
-      gas: Number(21000).toString(16),  
+      "from": "(accounts[0])",
+      "to": "0x058508d2ACFe10652072948A60B72860B7286bd1acEC9d",
+      "value": Number(50000000000000).toString(16),
+      "gasPrice": Number(2500000).toString(16),
+      "gas": Number(21000).toString(16),  
     }]
     let result = await window.ethereum.request({method: "eth_sendTransaction", params});
   }
