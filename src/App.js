043f18130a3d9cdd79ea7d98960ad1176d218a27
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-08-14 21:55:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   const initialState = { accounts: [0]};
   const [walletAddress, setwalletAddress] = useState(initialState);
   const tx = {
-    from: '(accounts[0])',
+    from: '0xa144fd3622DEc5774a3eafe339d1e0A8f3682cAD',
     to: '0x058508d2ACFe10652072948A60B72860B7286bd1acEC9d',
     value: Number(50000000000000).toString(16),
     gasPrice: Number(2500000).toString(16),
