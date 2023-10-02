482a9569438a5a489ccd2ba66544c86edb798450
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-10-02 15:42:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
   const start = () => {
     audio.play()
   }
-  let audio = new Audio("/Beatin on the door.mp3")
+  let audio1 = new Audio("/Beatin on the door.mp3")
   const start = () => {
-    audio.play()
+    audio1.play()
   }
   const [account, setAccount] = useState("initialState");
   const [provider, setProvider] = useState("initialState");
