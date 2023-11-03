4a3b6416d42b59cf1a688b420d62a2acff285537
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-11-03 12:19:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function App() {
         <button onClick={connectWallet}>Connect Wallet</button>
         </p>
         <p>      
-        <button onClick={start}>Playy</button>
+        <button onClick={start}>Play</button>
         </p>
         <img src="/Otter.png" alt=""/>
         <p>
